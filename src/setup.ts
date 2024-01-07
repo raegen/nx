e4beb187651b3c92a7c2bd710990861a49a427c1
Nikola Pavlovic <npavlovic101@gmail.com>
2024-01-07 17:36:59 +0100

    run as su

--- src/setup.ts
+++ src/setup.ts
@@ -2,7 +2,7 @@ import { execSync } from 'node:child_process'
 import { dirname, resolve } from 'node:path'
 
 execSync(
-  `ln -s "${process.execPath}" /usr/bin/node && ${resolve(
+  `sudo ln -s "${process.execPath}" /usr/bin/node && ${resolve(
     dirname(process.execPath),
     'npm'
   )} install`,
