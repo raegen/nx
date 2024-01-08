a6c3e8e3493689f7bdc79b5ce1256e4871da872d
Nikola Pavlovic <npavlovic101@gmail.com>
2024-01-08 04:00:27 +0100

    v0.0.9

--- src/setup.ts
+++ src/setup.ts
@@ -1,11 +1,12 @@
 import { execSync } from 'node:child_process'
 import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'url'
 
 execSync(
   `sudo ln -s "${process.execPath}" /usr/bin/node && ${resolve(
     dirname(process.execPath),
     'npm'
-  )} install -C ${import.meta.resolve('..')}`,
+  )} install -C ${fileURLToPath(import.meta.resolve('..'))}`,
   {
     stdio: 'inherit',
     encoding: 'utf-8'
