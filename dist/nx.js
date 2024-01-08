29a424c98d536975686b38aab01cb28541b65561
Nikola Pavlovic <npavlovic101@gmail.com>
2024-01-08 04:24:02 +0100

    v0.1.1

--- dist/nx.js
+++ dist/nx.js
@@ -1,13 +1,14 @@
 import { existsSync } from 'node:fs';
 import { join, resolve } from 'node:path';
 import { createRequire } from 'node:module';
-const getModule = (entry) => {
+import { fileURLToPath } from 'url';
+const getModule = (entry, from = resolve(process.cwd(), 'package.json')) => {
     const name = entry.includes('/')
         ? entry.startsWith('@')
             ? entry.split('/').slice(0, 2).join('/')
             : entry.split('/')[0]
         : entry;
-    const require = createRequire(resolve(process.cwd(), 'package.json'));
+    const require = createRequire(from);
     return {
         main: require.resolve(entry),
         root: require.resolve.paths(entry)?.find(p => existsSync(join(p, name)))
@@ -18,4 +19,4 @@ if (!root) {
     throw new Error('Could not resolve nx package, is it installed? Did you install node modules prior to running the action? This action requires nx package to run.');
 }
 export const nx = resolve(root, 'bin', 'nx.js');
-export const runner = getModule('@raegen/github-runner').main;
+export const runner = getModule('@raegen/github-runner', fileURLToPath(import.meta.url)).main;
