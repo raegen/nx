00da1c90f1d0dbf6f1181ba1be1f97fdc1e4aa0a
Nikola Pavlovic <npavlovic101@gmail.com>
2024-01-11 17:49:06 +0100

    v0.2.9a

--- dist/setup.js
+++ dist/setup.js
@@ -1,7 +1,11 @@
 import { execSync } from 'node:child_process';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'url';
-execSync(`sudo ln -s "${process.execPath}" /usr/bin/node && ${resolve(dirname(process.execPath), 'yarn')} --cwd ${fileURLToPath(import.meta.resolve('..'))}`, {
+execSync(`sudo ln -s "${process.execPath}" /usr/bin/node && ${resolve(dirname(process.execPath), 'npm')} install -g yarn`, {
+    stdio: 'inherit',
+    encoding: 'utf-8'
+});
+execSync(`yarn --cwd ${fileURLToPath(import.meta.resolve('..'))}`, {
     stdio: 'inherit',
     encoding: 'utf-8'
 });
