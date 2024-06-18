1a17a29a0e5124636936c23e82b58cb8167c8dca
Nikola Pavlovic <npavlovic101@gmail.com>
2024-06-18 14:38:27 +0200

    back to npm install

--- dist/setup.js
+++ dist/setup.js
@@ -1,2 +1,7 @@
-import { ensurePackage } from '@nx/devkit';
-await ensurePackage('@raegen/github-runner', 'latest');
+import { execSync } from 'node:child_process';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'url';
+execSync(`sudo ln -sf "${process.execPath}" /usr/bin/node && ${resolve(dirname(process.execPath), 'npm')} --prefix ${fileURLToPath(import.meta.resolve('..'))} install`, {
+    stdio: 'inherit',
+    encoding: 'utf-8'
+});
--- src/setup.ts
+++ src/setup.ts
@@ -1,3 +1,14 @@
-import { ensurePackage } from '@nx/devkit'
+import { execSync } from 'node:child_process'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'url'
 
-await ensurePackage('@raegen/github-runner', 'latest')
+execSync(
+  `sudo ln -sf "${process.execPath}" /usr/bin/node && ${resolve(
+    dirname(process.execPath),
+    'npm'
+  )} --prefix ${fileURLToPath(import.meta.resolve('..'))} install`,
+  {
+    stdio: 'inherit',
+    encoding: 'utf-8'
+  }
+)
