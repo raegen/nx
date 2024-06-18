3a242c8c86d5ce1e5ec797dc5ab2274da612f51d
Nikola Pavlovic <npavlovic101@gmail.com>
2024-06-18 12:55:04 +0200

    Use npm instead of yarn

--- src/setup.ts
+++ src/setup.ts
@@ -1,19 +1,3 @@
-import { execSync } from 'node:child_process'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'url'
+import { ensurePackage } from '@nx/devkit'
 
-execSync(
-  `sudo ln -sf "${process.execPath}" /usr/bin/node && ${resolve(
-    dirname(process.execPath),
-    'npm'
-  )} install -g yarn`,
-  {
-    stdio: 'inherit',
-    encoding: 'utf-8'
-  }
-)
-
-execSync(`yarn --cwd ${fileURLToPath(import.meta.resolve('..'))}`, {
-  stdio: 'inherit',
-  encoding: 'utf-8'
-})
+await ensurePackage('@raegen/github-runner', 'latest')
